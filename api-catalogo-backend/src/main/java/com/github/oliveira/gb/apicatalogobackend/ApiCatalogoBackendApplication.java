9e673f412058abfb395cd32f5962850fc0d4cbc8
Gabriel de Oliveira <gabriel.olliveira.95@gmail.com>
2025-11-20 10:09:34 -0300

    chore/subindo as primeiras configurações, do banco de dados

---
+++ api-catalogo-backend/src/main/java/com/github/oliveira/gb/apicatalogobackend/ApiCatalogoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.oliveira.gb.apicatalogobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCatalogoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCatalogoBackendApplication.class, args);
+    }
+
+}
