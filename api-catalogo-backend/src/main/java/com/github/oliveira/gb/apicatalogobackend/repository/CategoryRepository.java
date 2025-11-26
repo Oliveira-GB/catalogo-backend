d090140e28a59b3ea755b9606004568c8ca047bf
Gabriel de Oliveira <gabriel.olliveira.95@gmail.com>
2025-11-25 22:27:06 -0300

    feat:Comecando a implementar o save do category

--- api-catalogo-backend/src/main/java/com/github/oliveira/gb/apicatalogobackend/repository/CategoryRepository.java
+++ api-catalogo-backend/src/main/java/com/github/oliveira/gb/apicatalogobackend/repository/CategoryRepository.java
@@ -1,9 +1,12 @@
 package com.github.oliveira.gb.apicatalogobackend.repository;
 
+import com.github.oliveira.gb.apicatalogobackend.dto.CategoryRequestDTO;
 import com.github.oliveira.gb.apicatalogobackend.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CategoryRepository extends JpaRepository<Category, UUID> {
+    Optional<Category> findByName(String name);
 }
